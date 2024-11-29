4921c2043d5a6f350b2f85f603ed01cad082d68b
SteliosKallinikou <stelios.kallinikou@akelius.com>
2024-11-29 10:33:02 +0200

    using Ng for loop

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Housinglocation} from '../interfaces/housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+@Input() housingLocation!: Housinglocation;
 }
